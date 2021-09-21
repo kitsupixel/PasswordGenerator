c9dadef19e89460f71ef7c83e20118f944d5b424
Nelson Costa <nelson.costa@pintocruz.pt>
2021-09-21 15:27:11 +0100

    Added default values on install

--- background.js
+++ background.js
@@ -1,5 +1,19 @@
+// Initialize storage with default values
+chrome.runtime.onInstalled.addListener(() => {
+    setUpDefaultValues();
+});
+
 
 // Clears the last password
 chrome.alarms.onAlarm.addListener(() => {
     chrome.storage.sync.remove("lastPassword");
 });
+
+const setUpDefaultValues = () => {
+    chrome.storage.sync.set({ "incAlphaLower": true });
+    chrome.storage.sync.set({ "incAlphaUpper": true });
+    chrome.storage.sync.set({ "incNumbers": true });
+    chrome.storage.sync.set({ "incSymbols": true });
+    chrome.storage.sync.set({ "passwordLength": 16 });
+}
+
